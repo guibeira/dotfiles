fab68cdd3e79b644bf7a1d11fbd567f5478b65c8
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2022-06-23 10:30:38 -0300

    Merge main

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -9,12 +9,15 @@ an executable
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 -- general
-lvim.builtin.sell_soul_to_devel = true
 lvim.log.level = "warn"
-lvim.format_on_save = true
+lvim.format_on_save = false
 lvim.colorscheme = "tokyonight"
 vim.opt.spell = true
 vim.opt.spelllang = "en"
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer" })
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
+
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
@@ -50,17 +53,18 @@ lvim.keys.normal_mode["<S-T>"] = ":tabnew<cr>"
 --   name = "+Trouble",
 --   r = { "<cmd>Trouble lsp_references<cr>", "References" },
 --   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
+--   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
 --   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
 --   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
+--   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
 -- }
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
+lvim.builtin.alpha.active = false
+lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
-lvim.builtin.terminal.active = true
+lvim.builtin.terminal.active = false
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
 
@@ -88,13 +92,17 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- ---@usage disable automatic installation of servers
 -- lvim.lsp.automatic_servers_installation = false
 
--- ---@usage Select which servers should be configured manually. Requires `:LvimCacheRest` to take effect.
--- See the full default list `:lua print(vim.inspect(lvim.lsp.override))`
--- vim.list_extend(lvim.lsp.override, { "pyright" })
-
--- ---@usage setup a server -- see: https://www.lunarvim.org/languages/#overriding-the-default-configuration
+-- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+-- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
 -- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pylsp", opts)
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
+-- ---`:LvimInfo` lists which server(s) are skiipped for the current filetype
+-- vim.tbl_map(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
 
 -- -- you can set a custom on_attach function that will be used for all the language servers
 -- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
@@ -142,21 +150,21 @@ lvim.builtin.treesitter.highlight.enabled = true
 
 -- Additional Plugins
 lvim.plugins = {
-      {
-        "folke/persistence.nvim",
-          event = "BufReadPre", -- this will only start session saving when an actual file was opened
-          module = "persistence",
-          config = function()
-            require("persistence").setup {
-              dir = vim.fn.expand(vim.fn.stdpath "config" .. "/session/"),
-              options = { "buffers", "curdir", "tabpages", "winsize" },
-            }
-        end,
-      },
-    {
-      "karb94/neoscroll.nvim",
-      event = "WinScrolled",
-      config = function()
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    module = "persistence",
+    config = function()
+      require("persistence").setup {
+        dir = vim.fn.expand(vim.fn.stdpath "config" .. "/session/"),
+        options = { "buffers", "curdir", "tabpages", "winsize" },
+      }
+    end,
+  },
+  {
+    "karb94/neoscroll.nvim",
+    event = "WinScrolled",
+    config = function()
       require('neoscroll').setup({
             -- All these keys will be mapped to their corresponding default scrolling animation
             mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
@@ -171,54 +179,6 @@ lvim.plugins = {
             post_hook = nil,              -- Function to run after the scrolling animation ends
             })
       end
-    },
-    {
-        "ruifm/gitlinker.nvim",
-        event = "BufRead",
-        config = function()
-        require("gitlinker").setup {
-              opts = {
-                -- remote = 'github', -- force the use of a specific remote
-                  -- adds current line nr in the url for normal mode
-                  add_current_line_on_normal_mode = true,
-                -- callback for what to do with the url
-                  action_callback = require("gitlinker.actions").open_in_browser,
-                -- print the url after performing the action
-                  print_url = false,
-                -- mapping to call url generation
-                  mappings = "<leader>gy",
-              },
-            }
-        end,
-        requires = "nvim-lua/plenary.nvim",
-    },
-    {
-      "p00f/nvim-ts-rainbow",
-    },
-    {
-      "npxbr/glow.nvim",
-      ft = {"markdown"}
-    },
-    {
-      "norcalli/nvim-colorizer.lua",
-        config = function()
-          require("colorizer").setup({ "*" }, {
-              RGB = true, -- #RGB hex codes
-              RRGGBB = true, -- #RRGGBB hex codes
-              RRGGBBAA = true, -- #RRGGBBAA hex codes
-              rgb_fn = true, -- CSS rgb() and rgba() functions
-              hsl_fn = true, -- CSS hsl() and hsla() functions
-              css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-              css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-              })
-      end,
-    },
-    {
-      "windwp/nvim-ts-autotag",
-      event = "InsertEnter",
-      config = function()
-        require("nvim-ts-autotag").setup()
-      end,
     },
     {
       "tzachar/cmp-tabnine",
@@ -236,35 +196,110 @@ lvim.plugins = {
     --     vim.cmd ("let g:minimap_auto_start_win_enter = 1")
     --   end,
     -- },
-    {
-      "sindrets/diffview.nvim",
-      event = "BufRead",
-    },
-    {"folke/tokyonight.nvim"},
-    {
-      "gelfand/copilot.vim",
-      disable = not lvim.builtin.sell_soul_to_devel,
-      config = function ()
-        -- copilot assume mapped
-        vim.g.copilot_assume_mapped = true
-        vim.g.copilot_no_tab_map = true
-      end
-    },
-    {
-      "hrsh7th/cmp-copilot",
-      disable = not lvim.builtin.sell_soul_to_devel,
-      config = function ()
-        lvim.builtin.cmp.formatting.source_names["copilot"] = "(Cop)"
-        table.insert(lvim.builtin.cmp.sources, {name = "copilot"})
-      end
-    },
-}
+  {
+    "ruifm/gitlinker.nvim",
+    event = "BufRead",
+    config = function()
+      require("gitlinker").setup {
+        opts = {
+          -- remote = 'github', -- force the use of a specific remote
+          -- adds current line nr in the url for normal mode
+          add_current_line_on_normal_mode = true,
+          -- callback for what to do with the url
+          action_callback = require("gitlinker.actions").open_in_browser,
+          -- print the url after performing the action
+          print_url = false,
+          -- mapping to call url generation
+          mappings = "<leader>gy",
+        },
+      }
+    end,
+    requires = "nvim-lua/plenary.nvim",
+  },
+  {
+    "p00f/nvim-ts-rainbow",
+  },
+  {
+    "npxbr/glow.nvim",
+    ft = { "markdown" }
+  },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({ "*" }, {
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      })
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  -- {
+  --   'wfxr/minimap.vim',
+  --   run = "cargo install --locked code-minimap",
+  --   -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
+  --   config = function ()
+  --     vim.cmd ("let g:minimap_width = 10")
+  --     vim.cmd ("let g:minimap_auto_start = 1")
+  --     vim.cmd ("let g:minimap_auto_start_win_enter = 1")
+  --   end,
+  -- },
+  {
+    "sindrets/diffview.nvim",
+    event = "BufRead",
+  },
+  { "folke/tokyonight.nvim" },
+  {
+    "gelfand/copilot.vim",
+    disable = not lvim.builtin.sell_soul_to_devel,
+    config = function()
+      -- copilot assume mapped
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_no_tab_map = true
+    end
+  },
+  {
+    "hrsh7th/cmp-copilot",
+    disable = not lvim.builtin.sell_soul_to_devel,
+    config = function()
+      lvim.builtin.cmp.formatting.source_names["copilot"] = "(Cop)"
+      table.insert(lvim.builtin.cmp.sources, { name = "copilot" })
+    end
+  },
 
-lvim.builtin.which_key.mappings["S"]= {
-  name = "Session",
-  c = { "<cmd>lua require('persistence').load()<cr>", "Restore last session for current dir" },
-  l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Restore last session" },
-  Q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
+  { "editorconfig/editorconfig-vim" },
+  {
+    "simrat39/rust-tools.nvim",
+    config = function()
+      local lsp_installer_servers = require "nvim-lsp-installer.servers"
+      local _, requested_server = lsp_installer_servers.get_server "rust_analyzer"
+      require("rust-tools").setup({
+        tools = {
+          autoSetHints = true,
+          hover_with_actions = true,
+          runnables = {
+            use_telescope = true,
+          },
+        },
+        server = {
+          cmd_env = requested_server._default_options.cmd_env,
+          on_attach = require("lvim.lsp").common_on_attach,
+          on_init = require("lvim.lsp").common_on_init,
+        },
+      })
+    end,
+    ft = { "rust", "rs" },
+  },
 }
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
