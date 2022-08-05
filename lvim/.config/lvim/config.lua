23bd2f997ba43becc0a7c20bb3a5b08bcf6b5fca
@guilherme.desouzavieira <guilherme.vieira.beira@gmail.com>
2022-08-05 20:13:32 -0300

    Update copilot command

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -261,6 +261,7 @@ lvim.plugins = {
   },
   {"github/copilot.vim"},
   { "folke/tokyonight.nvim" },
+  {"Mofiqul/dracula.nvim"},
   { "editorconfig/editorconfig-vim" },
   {
     "simrat39/rust-tools.nvim",
@@ -290,16 +291,14 @@ vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_tab_fallback = ""
 local cmp = require "cmp"
-lvim.builtin.cmp.mapping["<Tab>"] = function(fallback)
-  if cmp.visible() then
-    cmp.select_next_item()
+
+lvim.builtin.cmp.mapping["<C-e>"] = function(fallback)
+  cmp.mapping.abort()
+  local copilot_keys = vim.fn["copilot#Accept"]()
+  if copilot_keys ~= "" then
+    vim.api.nvim_feedkeys(copilot_keys, "i", true)
   else
-    local copilot_keys = vim.fn["copilot#Accept"]()
-    if copilot_keys ~= "" then
-      vim.api.nvim_feedkeys(copilot_keys, "i", true)
-    else
-      fallback()
-    end
+    fallback()
   end
 end
 
