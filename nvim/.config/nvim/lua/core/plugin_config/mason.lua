434c470626d935b216eb1fb2cbac91d5e064ce0a
guilhermevbeira <guilherme.vieira.beira@gmail.com>
2023-05-30 16:34:49 -0300

    Use auto install

--- nvim/.config/nvim/lua/core/plugin_config/mason.lua
+++ nvim/.config/nvim/lua/core/plugin_config/mason.lua
@@ -1,19 +1,4 @@
 require("mason").setup()
 require("mason-lspconfig").setup {
-    ensure_installed = {
-     "bash-language-server",
-     "css-lsp",
-     "dockerfile-language-server",
-     "html-lsp",
-     "json-lsp",
-     "lemminx",
-     "lua-language-server",
-     "pyright",
-     "rust-analyzer",
-     "tailwindcss-language-server",
-     "typescript-language-server",
-     "vim-language-server",
-     "vue-language-server",
-     "yaml-language-server",
-  },
+  auto_install = true,
 }
