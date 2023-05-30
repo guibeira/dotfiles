5d5a9d209fd0897d9b629ba783f70fc80f1b4cc1
guilhermevbeira <guilherme.vieira.beira@gmail.com>
2023-05-30 17:29:12 -0300

    Update tsserver config

--- nvim/.config/nvim/lua/core/plugin_config/lsp_config.lua
+++ nvim/.config/nvim/lua/core/plugin_config/lsp_config.lua
@@ -42,3 +42,7 @@ require("lspconfig").solargraph.setup {
 require("lspconfig").pyright.setup {
   capabilities = capabilities,
 }
+
+require("lspconfig").tsserver.setup {
+  capabilities = capabilities,
+}
