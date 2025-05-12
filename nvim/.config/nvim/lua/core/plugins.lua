f80d5bac877ee2db77fb983a098f33e824c2aa82
Guilherme Beira <guilherme.vieira.beira@gmail.com>
2025-05-12 19:46:26 -0300

    Update avante config

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -286,7 +286,7 @@ local plugins = {
 		opts = {
 			-- -- add any opts here
 			-- -- for example
-			-- provider = "openai",
+			provider = "copilot",
 			-- openai = {
 			-- 	endpoint = "https://api.openai.com/v1",
 			-- 	model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
