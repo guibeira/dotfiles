d3c8fa07ce23197162ce70397021b7c65edaed7b
guibeira <contact@guibeira.dev>
2026-08-25 13:39:46 -0300

    Show gitignore files

--- nvim/.config/nvim/lua/core/plugin_config/nvim-tree.lua
+++ nvim/.config/nvim/lua/core/plugin_config/nvim-tree.lua
@@ -1,29 +1,32 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 local function my_on_attach(bufnr)
-	local api = require("nvim-tree.api")
+  local api = require("nvim-tree.api")
 
-	local function opts(desc)
-		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-	end
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
 
-	api.config.mappings.default_on_attach(bufnr)
+  api.config.mappings.default_on_attach(bufnr)
 
-	vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
-	vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
-	-- open vertical split
-	vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
-	-- open horizontal split
-	vim.keymap.set("n", "s", api.node.open.horizontal, opts("Open: Horizontal Split"))
-	vim.keymap.del("n", "<C-k>", { buffer = bufnr })
-	vim.keymap.set("n", "<S-k>", api.node.open.preview, opts("Open Preview"))
+  vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+  vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+  -- open vertical split
+  vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+  -- open horizontal split
+  vim.keymap.set("n", "s", api.node.open.horizontal, opts("Open: Horizontal Split"))
+  vim.keymap.del("n", "<C-k>", { buffer = bufnr })
+  vim.keymap.set("n", "<S-k>", api.node.open.preview, opts("Open Preview"))
 end
 
 require("nvim-tree").setup({
-	on_attach = my_on_attach,
-	view = {
-		adaptive_size = false,
-	},
+  on_attach = my_on_attach,
+  filters = {
+    git_ignored = false,
+  },
+  view = {
+    adaptive_size = false,
+  },
 })
 
 vim.keymap.set("n", "<c-n>", ":NvimTreeFindFileToggle<CR>")
